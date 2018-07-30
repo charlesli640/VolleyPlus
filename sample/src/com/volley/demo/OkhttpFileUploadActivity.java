9b55023fe56fa1fcbcd90a18c83e726756b930c2
Charles Li <charles.li@magnet.com>
2018-07-30 10:44:13 -0700

    Retry, but seems okhttp cannot upload file

--- sample/src/com/volley/demo/OkhttpFileUploadActivity.java
+++ sample/src/com/volley/demo/OkhttpFileUploadActivity.java
@@ -8,8 +8,10 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -28,6 +30,8 @@ import android.widget.Toast;
 import com.volley.demo.util.Utils;
 import com.volley.demo.misc.FileInfo;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
@@ -56,6 +60,8 @@ public class OkhttpFileUploadActivity extends AppCompatActivity {
     private Button btnClose;
     private ProgressBar progressBar;
     private TextView progressBarMessage;
+    private int retried = 0;
+    private FileInfo fileInfo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -65,9 +71,9 @@ public class OkhttpFileUploadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_upload);
 
-        lblLog = (TextView) findViewById(R.id.lbl_file_upload_log);
-        btnFileUpload = (Button) findViewById(R.id.btn_file_upload);
-        btnClose = (Button) findViewById(R.id.btn_file_close);
+        lblLog = findViewById(R.id.lbl_file_upload_log);
+        btnFileUpload = findViewById(R.id.btn_file_upload);
+        btnClose = findViewById(R.id.btn_file_close);
         etUrl = findViewById(R.id.url);
 
         progressBarMessage = findViewById(R.id.progressBarMessage);
@@ -79,6 +85,7 @@ public class OkhttpFileUploadActivity extends AppCompatActivity {
                 Intent intent = getFileChooserIntent();
                 startActivityForResult(intent, READ_REQUEST_CODE);
                 lblLog.setVisibility(View.GONE);
+                retried = 0;
             }
         });
 
@@ -109,8 +116,6 @@ public class OkhttpFileUploadActivity extends AppCompatActivity {
             Uri uri = null;
             if (data != null) {
                 uri = data.getData();
-                //final FileInfo fileInfo = new FileInfo(this, uri);
-                //uploadFile_Okhttp(fileInfo);
                 uploadFileTask(this, uri);
             } else {
                 lblLog.setVisibility(View.VISIBLE);
@@ -127,35 +132,67 @@ public class OkhttpFileUploadActivity extends AppCompatActivity {
         class UploadProgressTask extends AsyncTask<String, Void, String> {
             @Override
             protected String doInBackground(String... params) {
-                final FileInfo fileInfo = new FileInfo(ctx, uri);
-                String result = uploadFile_Okhttp(fileInfo);
-                //uploadFile_Volleyplus(fileInfo);
-                return result;
+                fileInfo = new FileInfo(ctx, uri);
+                // Check the file size
+                if(!fileInfo.isValidSize())
+                    return "Not valid size";
+                if(!fileInfo.isValidExtension())
+                    return "Not valid type";
+
+                showProgress();
+                return uploadFile_Okhttp(ctx, fileInfo);  // If using async callback, it always fails
+                //return null;
+                //return result;
             }
 
             @Override
             protected void onPostExecute(String result) {
-                progressBarMessage.setVisibility(View.GONE);
-                progressBar.setVisibility(View.GONE);
+                if (result != null)
+                    hideProgress(result);
+                if (retried <= 2) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+                    builder.setMessage("Are you sure you want to retry?")
+                            .setCancelable(false)
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    retried++;
+                                    Log.d(TAG, "retry #" + retried);
+                                    uploadFile_Okhttp(ctx, fileInfo);
+                                    showProgress();
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                    Log.d(TAG, "User don't want to retry");
+                                    String err = getString(R.string.file_upload_error_text);
+                                    //hideSpinner(err);
+                                    //sendCustomerMessage(err, err);
+                                    retried = 0;
+                                    Toast.makeText(ctx, err, Toast.LENGTH_LONG).show();
+                                }
+                            });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
 
-                lblLog.setVisibility(View.VISIBLE);
-                lblLog.setText(result);
             }
 
-            @Override
-            protected void onPreExecute() {
-                progressBarMessage.setText(getString(R.string.file_upload_progress_title, 0));
-                progressBarMessage.setVisibility(View.VISIBLE);
-                progressBar.setVisibility(View.VISIBLE);
-                setProgress(0, 100);
-            }
+//            @Override
+//            protected void onPreExecute() {
+//                progressBarMessage.setText(getString(R.string.file_upload_progress_title, 0));
+//                progressBarMessage.setVisibility(View.VISIBLE);
+//                progressBar.setVisibility(View.VISIBLE);
+//                setProgress(0, 100);
+//            }
         }
 
         UploadProgressTask uploadfile = new UploadProgressTask();
         uploadfile.execute();
     }
 
-    private String uploadFile_Okhttp(final FileInfo fileInfo) {
+    private String uploadFile_Okhttp(final Context ctx, final FileInfo fileInfo) {
+        boolean success = false;
         String result = getString(R.string.file_upload_error_text);
         final ProgressListener progressListener = new ProgressListener() {
 
@@ -168,7 +205,7 @@ public class OkhttpFileUploadActivity extends AppCompatActivity {
             }
         };
 
-        String url = "http://192.168.1.100:5000/";
+        String url = "http://192.168.1.9:5000/";
         //String url = "http://10.0.2.2:5000/";
         //String url = "https://ft-west.touchcommerce.com/filetransfer/rest/cont/uploadFile";
 
@@ -190,10 +227,9 @@ public class OkhttpFileUploadActivity extends AppCompatActivity {
                 Log.d("RequestBodyInner", "writeTo");
                 InputStream is = null;
                 try {
-                    is = contentResolver.openInputStream(fileInfo.uri); //fd.createInputStream();
-                    Log.d("RequestBodyInner", "sink.writeAll");
+                    is = contentResolver.openInputStream(fileInfo.uri);
+                    //Log.d("RequestBodyInner", "sink.writeAll");
                     sink.writeAll(Okio.buffer(Okio.source(is)));
-                    //is.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 } finally {
@@ -219,8 +255,33 @@ public class OkhttpFileUploadActivity extends AppCompatActivity {
                 .post(requestBody)
                 .build();
 
+        /*
+        Callback callback = new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String err = getString(R.string.file_upload_error_text);
+                //lblLog.setText(getString(R.string.file_upload_error_text));
+                //sendSystemMessageToAgent(err);
+                hideProgress(err);
+                Log.i(TAG, "OKHttp call:" + call.toString() + " Callback @@@" + e.toString());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String data = response.body().string();
+                try {
+                    JSONObject jo = new JSONObject(data);
+                    String filename = jo.getString("fileName");
+                    //sendCustomerMessage(generateFileUrl(filename));
+                } catch (JSONException e) {
+                    Log.e(TAG, "onResponse json error:" + e.getMessage());
+                }
+                hideProgress("");
+            }
+        };*/
+
         OkHttpClient client = new OkHttpClient.Builder()
-                .readTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
                 .build();
         //client.newCall(request).enqueue(callback);
 
@@ -237,7 +298,7 @@ public class OkhttpFileUploadActivity extends AppCompatActivity {
                     Log.e(TAG, "onResponse json error:" + e.getMessage());
                 }
                 result = data;
-
+                success = true;
             } else {
                 // retry
                 String err = getString(R.string.file_upload_error_text);
@@ -253,7 +314,37 @@ public class OkhttpFileUploadActivity extends AppCompatActivity {
         }
         Log.d(TAG, "return: " + result);
 
+        if(!success) {
+            // post message of failure to let the main thread create this
+
+        }
         return result;
+    }
+
+    private void showProgress() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                progressBarMessage.setText(getString(R.string.file_upload_progress_title, 0));
+                progressBarMessage.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+                setProgress(0, 100);
+            }
+        });
+    }
+
+    private void hideProgress(final String result) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                progressBarMessage.setVisibility(View.GONE);
+                progressBar.setVisibility(View.GONE);
+
+                lblLog.setVisibility(View.VISIBLE);
+                lblLog.setText(result);
+            }
+        });
+
     }
 
     private void setProgress(final int trans, final int total) {
